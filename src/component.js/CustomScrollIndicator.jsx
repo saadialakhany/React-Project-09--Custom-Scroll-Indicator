f581fc357593da237a352aae2e32c2ae3459ab23
saadialakhany <146756524+saadialakhany@users.noreply.github.com>
2024-05-28 16:57:28 +0400

    final commit

--- src/component.js/CustomScrollIndicator.jsx
+++ src/component.js/CustomScrollIndicator.jsx
@@ -4,6 +4,7 @@ import "./style.css";
 const CustomScrollIndicator = () => {
   const [productList, setProductList] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [scrollPercentage, setScrollPercentage] = useState(0);
 
   const fetchData = async () => {
     try {
@@ -23,16 +24,48 @@ const CustomScrollIndicator = () => {
     }
   };
 
+  const handleScrollPercentage = () => {
+    // console.log(
+    //   document.body.scrollTop,
+    //   document.documentElement.scrollTop,
+    //   document.documentElement.scrollHeight,
+    //   document.documentElement.clientHeight
+    // );
+    const howMuchScrolled =
+      document.body.scrollTop || document.documentElement.scrollTop;
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+    setScrollPercentage((howMuchScrolled / height) * 100);
+  };
+
   useEffect(() => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    window.addEventListener("scroll", handleScrollPercentage);
+
+    return () => {
+      window.removeEventListener("scroll", () => {});
+    };
+  }, []);
+
+  if (loading) return <div>Data is Loading. Please wait!</div>;
+
+  //   console.log(scrollPercentage);
   return (
     <>
       <div className="scroll--container">
         <h1>Custom Scroll Indicator</h1>
+        <div className="scroll-bar">
+          <div
+            className="current-progress-bar"
+            style={{ width: `${scrollPercentage}%` }}
+          ></div>
+        </div>
       </div>
-      <div className="scroll-bar"></div>
 
       <div className="list--container">
         {productList && productList.length > 0
